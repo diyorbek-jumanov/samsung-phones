2c29802fa12cb94f8083f94079296a34be2b2cd7
diyorbekjumanov <diyorbekjumanov1120@gmail.com>
2021-03-02 02:43:21 +0000

    for

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ imges_url = json.loads(open('data.json').read())
 
 app = Flask(__name__)
 
-@app.route('/home')
+@app.route('/')
 def home():
     
     return render_template('home.html', title='home', content=imges_url)
