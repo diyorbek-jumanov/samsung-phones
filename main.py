e6716d6f779191f28096386c2c02673009f77917
diyorbekjumanov <diyorbekjumanov1120@gmail.com>
2021-03-02 07:42:58 +0000

    decorators

--- main.py
+++ main.py
@@ -12,8 +12,71 @@ def home():
 
 @app.route('/s3')
 def S3():
-    title = 'S3'
+    title = 'Samsung Galaxy S3'
     data = imges_url[0]
+
+    return render_template('phone.html', title=title, data=data)
+    
+@app.route('/s4')
+def S4():
+    title = 'Samsung Galaxy S4'
+    data = imges_url[1]
+
+    return render_template('phone.html', title=title, data=data)
+    
+@app.route('/s5')
+def S5():
+    title = 'Samsung Galaxy S5'
+    data = imges_url[2]
+
+    return render_template('phone.html', title=title, data=data)
+    
+@app.route('/s6')
+def S6():
+    title = 'Samsung Galaxy S6'
+    data = imges_url[3]
+
+    return render_template('phone.html', title=title, data=data)
+    
+@app.route('/s7')
+def S7():
+    title = 'Samsung Galaxy S7'
+    data = imges_url[4]
+
+    return render_template('phone.html', title=title, data=data)
+    
+@app.route('/s8')
+def S8():
+    title = 'Samsung Galaxy S8'
+    data = imges_url[5]
+
+    return render_template('phone.html', title=title, data=data)
+    
+@app.route('/s9')
+def S9():
+    title = 'Samsung Galaxy S9'
+    data = imges_url[6]
+
+    return render_template('phone.html', title=title, data=data)
+    
+@app.route('/s10')
+def S10():
+    title = 'Samsung Galaxy S10'
+    data = imges_url[7]
+
+    return render_template('phone.html', title=title, data=data)
+    
+@app.route('/s20')
+def S20():
+    title = 'Samsung Galaxy S20'
+    data = imges_url[8]
+
+    return render_template('phone.html', title=title, data=data)
+    
+@app.route('/s21')
+def S21():
+    title = 'Samsung Galaxy S21'
+    data = imges_url[9]
 
     return render_template('phone.html', title=title, data=data)
     
